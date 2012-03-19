6fced766430834b900354a701db97c49ee5f1c16
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-03-19 10:51:12 +0100

    prepare for translations / auto redirect from manager-permission view to advanced-sharing if user has view only permission

--- ftw/permissionmanager/browser/permissionmanager.py
+++ ftw/permissionmanager/browser/permissionmanager.py
@@ -5,6 +5,7 @@ from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.workflow.browser.sharing import SharingView
 from Products.statusmessages.interfaces import IStatusMessage
+from Products.CMFCore.utils import getToolByName
 from ftw.permissionmanager import permission_manager_factory as _
 
 
@@ -12,6 +13,12 @@ class PermissionManager(BrowserView):
 
     def __call__(self, *args, **kwargs):
         self.request.set('disable_border', True)
+        portal_membership = getToolByName(self.context, 'portal_membership')
+        # Redirect ro advanced_sharing view, if user has read only permission
+        if not portal_membership.checkPermission(
+            'Sharing page: Delegate roles', self.context):
+            return self.context.restrictedTraverse('@@advanced-sharing')()
+
         return super(PermissionManager, self).__call__(*args, **kwargs)
 
 
