c01d8c5561b1c3ca93908909599f75a9cb4f760d
Jonas Baumann <jone@jone.ch>
2012-11-12 14:35:24 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ tests_require = ['plone.app.testing', ]
 setup(name='ftw.permissionmanager',
       version=version,
       description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -19,23 +20,27 @@ setup(name='ftw.permissionmanager',
         'Framework :: Plone :: 4.1',
         "Programming Language :: Python",
         ],
+
       keywords='',
-      author='%s, 4teamwork GmbH' % maintainer,
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://github.com/4teamwork/ftw.permissionmanager',
-      license='GPL',
+      url='https://github.com/4teamwork/ftw.permissionmanager',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'collective.monkeypatcher',
+        # -*- Extra requirements: -*-
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      install_requires=[
-          'setuptools',
-          'collective.monkeypatcher',
-          # -*- Extra requirements: -*-
-      ],
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
