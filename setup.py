d0472b152a738542038a924b37fbb61bd7d58a81
Jonas Baumann <jone@jone.ch>
2013-01-28 11:14:41 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -13,12 +13,14 @@ setup(name='ftw.permissionmanager',
           open(os.path.join("docs", "HISTORY.txt")).read(),
 
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.0',
         'Framework :: Plone :: 4.1',
-        "Programming Language :: Python",
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       keywords='',
