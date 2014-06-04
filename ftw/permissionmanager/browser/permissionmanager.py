98c14ada9bf0244ede84d5983f3cd3d2f4af9e8a
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-06-04 10:49:53 +0200

    No longer allow users with view permission to access the advanced security features.

--- ftw/permissionmanager/browser/permissionmanager.py
+++ ftw/permissionmanager/browser/permissionmanager.py
@@ -1,15 +1,8 @@
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
 
 
 class PermissionManager(BrowserView):
 
     def __call__(self, *args, **kwargs):
         self.request.set('disable_border', True)
-        portal_membership = getToolByName(self.context, 'portal_membership')
-        # Redirect ro advanced_sharing view, if user has read only permission
-        if not portal_membership.checkPermission(
-                'Sharing page: Delegate roles', self.context):
-            return self.context.restrictedTraverse('@@prinicpal_role_tree')()
-
         return super(PermissionManager, self).__call__(*args, **kwargs)
