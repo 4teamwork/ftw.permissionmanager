e9600d8c0df34c18ad2f3a7a41917cd8497d790d
Jonas Baumann <jone@jone.ch>
2016-02-26 17:30:03 +0100

    Refactor reindexObjectSecurity patch to be less invasive.
    The patch now extends the original implementation and does no longer
    reimplement it completely.
    
    Registering additional indexes is now done by patching the security
    indexes list.
    
    This change should add solr compatibility.

--- ftw/permissionmanager/patches.py
+++ ftw/permissionmanager/patches.py
@@ -1,48 +1,29 @@
-from Acquisition import aq_base
-from logging import WARNING
 from Products.Archetypes.config import TOOL_NAME
-from Products.Archetypes.log import log
 from Products.Archetypes.utils import isFactoryContained
 from Products.CMFCore.interfaces import ICatalogTool
 from Products.CMFCore.utils import getToolByName
 
 
 def permissionmanager_reindexObjectSecurity(self, skip_self=False):
+    # Execute standard object reindexing.
+    self._old_reindexObjectSecurity(skip_self=skip_self)
+
+    # The standard indexing does not update catalog metadata.
+    # We need to do that because we introduce security relevant
+    # catalog metadata in this package.
+
+    # In order to do that, we reindex the fast index "getId" of the
+    # current context, so that we can use `update_metadata=True`.
+    # We do not do that recursive because the metadata values
+    # are non-recursive.
     if isFactoryContained(self):
         return
     at = getToolByName(self, TOOL_NAME, None)
     if at is None:
         return
 
-    catalogs = [c for c in at.getCatalogsByType(self.meta_type)
-                if ICatalogTool.providedBy(c)]
-    path = '/'.join(self.getPhysicalPath())
+    catalogs = [catalog for catalog in at.getCatalogsByType(self.meta_type)
+                if ICatalogTool.providedBy(catalog)]
 
     for catalog in catalogs:
-        for brain in catalog.unrestrictedSearchResults(path=path):
-            brain_path = brain.getPath()
-            if brain_path == path and skip_self:
-                continue
-
-            # Get the object
-            if hasattr(aq_base(brain), '_unrestrictedGetObject'):
-                ob = brain._unrestrictedGetObject()
-            else:
-                # BBB: Zope 2.7
-                ob = self.unrestrictedTraverse(brain_path, None)
-            if ob is None:
-                # BBB: Ignore old references to deleted objects.
-                # Can happen only in Zope 2.7, or when using
-                # catalog-getObject-raises off in Zope 2.8
-                log("reindexObjectSecurity: Cannot get %s from catalog" %
-                    brain_path, level=WARNING)
-                continue
-            # PATCH: Append principal_with_local_roles index to security
-            # relevant indexes.
-            indexes = list(self._cmf_security_indexes)
-            indexes.append('principal_with_local_roles')
-            # Recatalog with the same catalog uid.
-            # PATCH: update_metadata=1, origin ist update_metadata=0.
-            # We add get_local_roles and isLocalRoleAcquired to the metadata
-            catalog.reindexObject(ob, idxs=tuple(indexes),
-                                  update_metadata=1, uid=brain_path)
+        catalog.reindexObject(self, idxs=['getId'], update_metadata=True)
