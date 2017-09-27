770bc849918cb5d37b2bd3426e1b6fa459a8e7ac
Lukas Knoepfel <shylux@gmail.com>
2017-09-27 09:28:03 +0200

    Enable sharing listing for groups without introspection.

--- ftw/permissionmanager/browser/sharing.py
+++ ftw/permissionmanager/browser/sharing.py
@@ -1,15 +1,16 @@
 from Acquisition import aq_inner
+from itertools import chain
+from plone.app.workflow import PloneMessageFactory as _
+from plone.app.workflow.browser.sharing import merge_search_results
 from plone.app.workflow.browser.sharing import SharingView as base
-from plone.app.workflow.interfaces import ISharingPageRole
-from plone.memoize.instance import memoize
+from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode, getSiteEncoding
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.component import getUtilitiesFor
+from Products.PlonePAS.plugins.autogroup import VirtualGroup
+from zope.component import getMultiAdapter
 from zope.component import getUtility
-from plone.registry.interfaces import IRegistry
-from plone.app.workflow.browser.sharing import merge_search_results
-from itertools import chain
+from zope.i18n import translate
 
 
 class SharingView(base):
@@ -75,6 +76,26 @@ class SharingView(base):
         return self._principal_search_results(search_for_principal,
             get_principal_by_id, get_principal_title, 'user', 'userid')
 
+    def group_search_results(self):
+        """Return search results for a query to add new groups.
+
+        Returns a list of dicts, as per role_settings().
+        """
+
+        def search_for_principal(hunter, search_term):
+            return merge_search_results(chain(*[hunter.searchGroups(**{field:search_term})
+                for field in ['id', 'title']]), 'groupid')
+
+        def get_principal_by_id(group_id):
+            portal_groups = getToolByName(self.context, 'portal_groups')
+            return portal_groups.getGroupById(group_id)
+
+        def get_principal_title(group, _):
+            title = getattr(group, 'title', None)
+            return title or group.getName()
+
+        return self._principal_search_results(search_for_principal,
+            get_principal_by_id, get_principal_title, 'group', 'groupid')
 
     def role_settings(self):
         """Only return current settings"""
@@ -186,3 +207,68 @@ class SharingView(base):
                 safe_unicode(y["title"], encoding).lower()))
 
         return current_settings
+
+    def _principal_search_results(self,
+                                  search_for_principal,
+                                  get_principal_by_id,
+                                  get_principal_title,
+                                  principal_type,
+                                  id_key):
+        """Return search results for a query to add new users or groups.
+
+        Returns a list of dicts, as per role_settings().
+
+        Arguments:
+            search_for_principal -- a function that takes an IPASSearchView and
+                a search string. Uses the former to search for the latter and
+                returns the results.
+            get_principal_by_id -- a function that takes a user id and returns
+                the user of that id
+            get_principal_title -- a function that takes a user and a default
+                title and returns a human-readable title for the user. If it
+                can't think of anything good, returns the default title.
+            principal_type -- either 'user' or 'group', depending on what kind
+                of principals you want
+            id_key -- the key under which the principal id is stored in the
+                dicts returned from search_for_principal
+        """
+        context = self.context
+
+        translated_message = translate(_(u"Search for user or group"),
+                                       context=self.request)
+        search_term = self.request.form.get('search_term', None)
+        if not search_term or search_term == translated_message:
+            return []
+
+        existing_principals = set([p['id'] for p in self.existing_role_settings()
+                                if p['type'] == principal_type])
+        empty_roles = dict([(r['id'], False) for r in self.roles()])
+        info = []
+
+        hunter = getMultiAdapter((context, self.request), name='pas_search')
+        for principal_info in search_for_principal(hunter, search_term):
+            principal_id = principal_info[id_key]
+            if principal_id not in existing_principals:
+                principal = get_principal_by_id(principal_id)
+                roles = empty_roles.copy()
+
+                if principal is None and principal_type == 'group':
+                    """ Discovery not implemented. Use a virtual group. """
+                    principal = VirtualGroup(
+                        principal_info['id'],
+                        principal_info['title'] if 'title' in principal_info else '',
+                        principal_info['description'] if 'description' in principal_info else '')
+
+                for r in principal.getRoles():
+                    if r in roles:
+                        roles[r] = 'global'
+                login = principal.getUserName()
+                if principal_type == 'group':
+                    login = None
+                info.append(dict(id = principal_id,
+                                 title = get_principal_title(principal,
+                                                             principal_id),
+                                 login = login,
+                                 type = principal_type,
+                                 roles = roles))
+        return info
